021f817760ccd98bee192140c98359e858e6f800
FrancescoSpena <francescospena@MrBeth.home-life.hub>
2021-10-31 20:33:44 +0100

    Delete ridondant file

--- sensor.hpp
+++
@@ -1,8 +0,0 @@
-
-
-
-class Sensor{
-    public:
-    virtual void sensor_init();
-    virtual void sensor_handle();
-};
--- sensors.hpp
+++
@@ -1,7 +0,0 @@
-
-#pragma once
-
-class Sensors{
-    virtual void sensor_init();
-    virtual void sensor_handle();
-};
